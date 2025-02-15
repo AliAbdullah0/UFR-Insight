4a83055ba695ac06a39fc9784420de1eea198238
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2025-02-15 18:40:42 +0500

    Added insights

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navigation from '../components/Navigation'
 import Footer from '../components/Footer'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const Layout = () => {
   return (
-    <>
+    <> 
+    <SpeedInsights/>
+    <Analytics/>
         <Navigation/>
         <main className='overflow-x-hidden'>
         <Outlet/>
